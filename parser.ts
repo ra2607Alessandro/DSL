6688325c17435080f3928cbd8feb20b123f15daf
ra2607Alessandro <2alessandro.aimar@gmail.com>
2025-11-27 08:29:51 +0100

    starting out with parsing

--- parser.ts
+++ parser.ts
@@ -1,9 +1,24 @@
 import { Program } from "./ast";
+import { Token, tokenizer } from "./lexing";
 
 export default class Parser {
+    private peek(){
+       return this
+    }
+    private advance(){
+        return this.peek().shift()!
+    }
 
-    public ProduceAst(source_code: string): Program{
+    private expect(){
 
     }
+
+    public ProduceAst(source_code: string): Program{
+       const code = tokenizer(source_code);
+       return this.parse_program(code)
+    }
     
+    private parse_program(tokens: Token[]): Program{
+
+    }
 }
