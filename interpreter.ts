68a0ee4f30dce109fc781d345051f76927b3ea7f
ra2607Alessandro <2alessandro.aimar@gmail.com>
2025-11-29 10:43:55 +0100

    yes man, we're doing it

--- interpreter.ts
+++ interpreter.ts
@@ -1,4 +1,4 @@
-import { Account_Types, AccountBlock, Program } from "./ast"; 
+import { Account_Types, AccountBlock, JournalBlock, Program, Transaction } from "./ast"; 
 import { AccountMetaData, LedgerMetadata } from "./ds";
 
 
@@ -9,7 +9,7 @@ class Interpreter {
 
    private process_account_blocks(block: AccountBlock){
       if(block.type !== "AccountBlock"){
-        throw new Error(`The required type is: 'AccountBlock', yours is:${console.log(type.type)}`)
+        throw new Error(`The required type is: 'AccountBlock', yours is:${console.log(block.type)}`)
       }
       if(block.accounts){
         let name : string = "";
@@ -17,8 +17,27 @@ class Interpreter {
         const md = {type: type ,isExplicit: true}  as AccountMetaData
         return this.accountRegistry[name] = md 
       }
+      else{
+        throw new Error("We can're register an account block without accounts")
+      }
+   }
+   
+   private process_journal_blok(block: JournalBlock){
+        if(block.type !== "JournalBlock"){
+           throw new Error(`The required type is: 'JournalBlock', yours is:${console.log(block.type)}`)
+        }
+        for (const txn of block.txns){
+            this.process_transaction(txn)
+        }
+
    }
 
+   private process_transaction(txn: Transaction){
+     if (txn.type !== "Transaction"){
+        throw new Error(`The required type is: 'Transaction', yours is:${console.log(txn.type)}`)
+     }
+     
+   }
 
    public Interpret(program: Program){
       for(const block of program.value){
@@ -26,7 +45,9 @@ class Interpreter {
         switch(block.type){
 
             case "AccountBlock":
-                return this.process_account_blocks(block as AccountBlock)
+                return this.process_account_blocks(block as AccountBlock);
+            case "JournalBlock":
+                return this.process_journal_blok(block as JournalBlock)
         }
       }
    }
